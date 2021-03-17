2ec8279ea916cb3307996f9b2d782f11091220a1
MistUnky <misterunknown128@gmail.com>
2021-03-17 14:28:34 -0400

    Add stock generator

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ function register_rwalker(nodegen, nodecopy)
 		tiles = {nodegen..".png"}
 	})
 	minetest.register_abm({
-		nodenames={"randomwalker:"..nodegen},
+		nodenames = {"randomwalker:"..nodegen},
 		interval = 1,
 		chance = 1,
 		action = function(pos)
@@ -39,3 +39,107 @@ register_rwalker("default_cobble", "default:cobble")
 register_rwalker("default_mossycobble", "default:mossycobble")
 register_rwalker("air", "air")
 register_rwalker("default_meselamp", "default:meselamp")
+
+
+minetest.register_node("randomwalker:stonkgen", {
+	description = "Stonks Generator",
+	tiles = {"randomwalker_stonks.png"}
+})
+minetest.register_node("randomwalker:stonkxplus", {
+	description = "Stonks X+",
+	tiles = {"randomwalker_stonks.png"}
+})
+minetest.register_node("randomwalker:stonkxminus", {
+	description = "Stonks X-",
+	tiles = {"randomwalker_stonks.png"}
+})
+minetest.register_node("randomwalker:stonkzplus", {
+	description = "Stonks Z+",
+	tiles = {"randomwalker_stonks.png"}
+})
+minetest.register_node("randomwalker:stonkzminus", {
+	description = "Stonks Z-",
+	tiles = {"randomwalker_stonks.png"}
+})
+
+minetest.register_abm({
+	nodenames = {"randomwalker:stonkxplus"},
+	interval = 1,
+	chance = 2, ---add some delay
+	catch_up = false,
+	action = function(pos)
+		if math.random(0,1) == 1 then
+			minetest.set_node({x=pos.x+1,y=pos.y+1,z=pos.z}, {name="randomwalker:stonkxplus"})
+			minetest.set_node(pos, {name="default:cloud"})
+		else
+			--if not pos.y == 0 then --0 is minimum for stocks because why not right
+				minetest.set_node({x=pos.x+1,y=pos.y-1,z=pos.z}, {name="randomwalker:stonkxplus"})
+				minetest.set_node(pos, {name="default:cloud"})
+			--end
+		end
+	end
+})
+minetest.register_abm({
+	nodenames = {"randomwalker:stonkxminus"},
+	interval = 1,
+	chance = 2,
+	catch_up = false,
+	action = function(pos)
+		if math.random(0,1) == 1 then
+			minetest.set_node({x=pos.x-1,y=pos.y+1,z=pos.z}, {name="randomwalker:stonkxminus"})
+			minetest.set_node(pos, {name="default:cloud"})
+		else
+			--if not pos.y == 0 then --0 is minimum for stocks because why not right
+				minetest.set_node({x=pos.x-1,y=pos.y-1,z=pos.z}, {name="randomwalker:stonkxminus"})
+				minetest.set_node(pos, {name="default:cloud"})
+			--end
+		end
+	end
+})
+minetest.register_abm({
+	nodenames = {"randomwalker:stonkzplus"},
+	interval = 1,
+	chance = 2,
+	catch_up = false,
+	action = function(pos)
+		if math.random(0,1) == 1 then
+			minetest.set_node({x=pos.x,y=pos.y+1,z=pos.z+1}, {name="randomwalker:stonkzplus"})
+			minetest.set_node(pos, {name="default:cloud"})
+		else
+			--if not pos.y == 0 then --0 is minimum for stocks because why not right
+				minetest.set_node({x=pos.x,y=pos.y-1,z=pos.z+1}, {name="randomwalker:stonkzplus"})
+				minetest.set_node(pos, {name="default:cloud"})
+			--end
+		end
+	end
+})
+minetest.register_abm({
+	nodenames = {"randomwalker:stonkzminus"},
+	interval = 1,
+	chance = 2,
+	catch_up = false,
+	action = function(pos)
+		if math.random(0,1) == 1 then
+			minetest.set_node({x=pos.x,y=pos.y+1,z=pos.z-1}, {name="randomwalker:stonkzminus"})
+			minetest.set_node(pos, {name="default:cloud"})
+		else
+			--if not pos.y == 0 then --0 is minimum for stocks because why not right
+				minetest.set_node({x=pos.x,y=pos.y-1,z=pos.z-1}, {name="randomwalker:stonkzminus"})
+				minetest.set_node(pos, {name="default:cloud"})
+			--end
+		end
+	end
+})
+minetest.register_abm({
+	nodenames = {"randomwalker:stonkgen"},
+	interval = 1,
+	chance = 2,
+	catch_up = false,
+	action = function(pos)
+		minetest.set_node(pos, {name="default:cloud"})
+		minetest.set_node({x=pos.x+1,y=pos.y,z=pos.z}, {name="randomwalker:stonkxplus"})
+		minetest.set_node({x=pos.x-1,y=pos.y,z=pos.z}, {name="randomwalker:stonkxminus"})
+		minetest.set_node({x=pos.x,y=pos.y,z=pos.z+1}, {name="randomwalker:stonkzplus"})
+		minetest.set_node({x=pos.x,y=pos.y,z=pos.z-1}, {name="randomwalker:stonkzminus"})
+	end
+})
